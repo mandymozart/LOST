3c9ced533c183a92af16e794f9f5e6d494da3d60
janik hotz <janikhotz@gmail.com>
2015-04-15 21:52:48 +0200

    first data models

---
+++ models/Genre.js
@@ -0,0 +1,15 @@
+var keystone = require('keystone');
+var Types    = keystone.Field.Types;
+
+/*
+ * Genre Model
+ */
+
+var Genre = keystone.List('Genre');
+
+Genre.add({
+    name     : {type:String, required:true, index:true, initial:''},
+    category : {type:String, required:true, index:true, initial:'' } 
+});
+
+Genre.register();
--- models/Profile.js
+++ models/Profile.js
@@ -8,8 +8,16 @@ var Types    = keystone.Field.Types;
 var Profile = new keystone.List('Profile');
 
 Profile.add({
-
-
+    user  : { type: Types.Relationship, ref: 'User', index: true },
+    name  : { type: String, required: true, index: true },
+    about : {
+                brief    : { type: Types.Html, wysiwig: true, height: 50 },
+                extended : { type: Types.Html, wysiwig: true, height: 300} 
+            }
+    //TODO socialLinks    : {type : undefined } 
+    //TODO souncloudLinks : {type : undefined }
+    //TODO videoLinks     : {type : undefined }
+    //TODO 
 });
 
 
