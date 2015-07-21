bb8ac725910a165ba206ce92af95976aaac0a6ca
Danny Develop <danny@shalomsalon.de>
2015-07-21 14:55:10 +0200

    negotiation styling

--- public/js/controllers/NegotiationDetailCtrl.js
+++ public/js/controllers/NegotiationDetailCtrl.js
@@ -7,6 +7,41 @@ app.controller('NegotiationDetailCtrl', function($scope, $localStorage, $http){
 	$scope.models.messageAreaText = '';
 	$scope.storage = $localStorage;
 
+    var monthNames = [
+        "Jan",
+        "Feb",
+        "Mar",
+        "Apr",
+        "May",
+        "Jun",
+        "Jul",
+        "Aug",
+        "Sep",
+        "Oct",
+        "Nov",
+        "Dec"
+    ]
+
+    $scope.getDay = function(){
+        return new Date($localStorage.selectedNegotiation.date).getUTCDate();
+    }
+    $scope.getMonth = function(){
+        return monthNames[new Date($localStorage.selectedNegotiation.date).getUTCMonth()];
+    }
+    $scope.getYear = function(){
+        return new Date($localStorage.selectedNegotiation.date).getUTCFullYear();
+    }
+    $scope.getHours = function(){
+        return new Date($localStorage.selectedNegotiation.date).getUTCHours();
+    }
+    $scope.getMinutes = function(){
+        return new Date($localStorage.selectedNegotiation.date).getUTCMinutes();
+    }
+    $scope.getSeconds = function(){
+        return new Date($localStorage.selectedNegotiation.date).getUTCSeconds();
+    }
+
+
 	$scope.sent = function(nm){
 		return nm.sender === $localStorage.profile._id;
 	}
