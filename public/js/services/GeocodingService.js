0b4798d58cef443a153f4cb55509c17bd0ba45c9
janik hotz <janikhotz@gmail.com>
2015-07-27 13:23:24 +0200

    geolocation service start

---
+++ public/js/services/GeocodingService.js
@@ -0,0 +1,20 @@
+'use strict'
+var app = angular.module('muriquee')
+
+app.factory('GeocodingService', function($http)
+
+	var url = 'http://nominatim.openstreetmap.org/search/';
+
+	var geocode = function(query){
+		var req = {
+
+		}
+	}
+	var reverseGeocode = function(lat, lon){
+
+	}
+	return {
+		geocode:geocode,
+		reverseGeocode:reverseGeocode
+	}
+})
